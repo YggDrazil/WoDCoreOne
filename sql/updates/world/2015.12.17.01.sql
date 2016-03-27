c9f2b06a6a87bdab9cc4d0c709d6203895170c7d
cooler-SAI <cooler19860212@yahoo.com>
2016-03-27 23:05:53 +0300

    ZeXDB 434.08 updates & "patch" SmartAI Lab 434.10

--- sql/updates/world/2015.12.17.01.sql
+++
@@ -1,1 +0,0 @@
-insert  into `npc_spellclick_spells`(`npc_entry`,`spell_id`,`cast_flags`,`user_type`) values (49340,85584,1,0);
--- sql/updates/world/2016_03_25_01.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `creature_template` SET `ScriptName` = 0 WHERE `entry` = 38910;
-UPDATE `creature_template` SET `ScriptName` = 0 WHERE `entry` = 49230;
-UPDATE `creature_template` SET `ScriptName` = 0 WHERE `entry` = 38895;
-UPDATE `creature_template` SET `ScriptName` = 0 WHERE `entry` = 49231;
